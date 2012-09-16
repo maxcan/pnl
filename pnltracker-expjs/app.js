81481e479e0d5b8b549d453e3580bdf6fc284b68
Max Cantor (MBPr) <i@cantor.mx>
2012-09-16 12:13:00 -0700

    got auth working right.  finally.

--- pnltracker-expjs/app.js
+++ pnltracker-expjs/app.js
@@ -17,26 +17,29 @@ everyauth.google
 .appId('903799978070.apps.googleusercontent.com')
 .appSecret('aUfnPV75qEtNjrhEaczfJu__')
 .scope('https://www.googleapis.com/auth/userinfo.email') // What you want access to
-.handleAuthCallbackError( function (req, res) {
+// .handleAuthCallbackError( function (req, res) {
   // If a user denies your app, Google will redirect the user to
   // /auth/google/callback?error=access_denied
   // This configurable route handler defines how you want to respond to
   // that.
   // If you do not configure this, everyauth renders a default fallback
   // view notifying the user that their authentication failed and why.
-})
+// })
 .findOrCreateUser( function (session, accessToken, accessTokenExtra, googleUserMetadata) {
   var promise = this.Promise();
-  Models.User.find({email:googleUserMetadata.email}, function(err,usr) {
-    if (err) throw err;
-    if (usr === null || usr == '') {
-      Models.User.create( { email: googleUserMetadata.email}
-        , function newUserCb(err, newUsr) { 
+  var newUserCb = function(err,newUsr) {
           if (err) throw err;
+          // console.log('fulfilling with new user : ' + newUsr);  // _DEBUG
           promise.fulfill(newUsr);
-          });
+  };
+  Models.User.findOne({email:googleUserMetadata.email}, function(err,usr) {
+    if (err) throw err;
+    if (usr) {
+      newUserCb(null, usr);
     } else { 
-      promise.fulfill(usr) ; 
+      // promise.fulfill(usr) ; 
+      console.log('new user, will create');  // _DEBUG
+      Models.User.create( { email: googleUserMetadata.email}, newUserCb);
     } 
   });
   return promise;
@@ -49,23 +52,42 @@ everyauth.google
 
 everyauth.everymodule.userPkey('_id');
 everyauth.everymodule.findUserById( function (userId, callback) {
-  Models.User.findById(userId, callback);
+  console.log('everyauth find by id: ' + userId);  // _DEBUG
+  Models.User.findById(userId, function(err,u) {
+    if (err) throw err;
+    console.log('everyauth found u: ' + u);  // _DEBUG
+    callback(err,u);
+  });
 });
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('ghrtiughdifuvsghuit4578ye'));
-  app.use(express.session());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'blalblsdfsdf'}));
+  app.use(express.bodyParser());
   app.use(everyauth.middleware());
+  app.use(function(req,res,next) {
+    console.log('about to print debug stuff');  // _DEBUG
+    try {
+      console.log('cookies:  ' + JSON.stringify(req.cookies));  // _DEBUG
+      console.log('session:  ' + JSON.stringify(req.session));  // _DEBUG
+      console.log('signed cookies:  ' + JSON.stringify(req.signedCookies));  // _DEBUG
+    } catch (e) {
+      console.log('excepitn: ' + e);  // _DEBUG
+    }
+    next();
+  });
+
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
+
 });
 
 app.configure('development', function(){
