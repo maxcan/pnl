53e3ed2a31a6f577377f522869d1f20d485b5a28
Max Cantor (MBPr) <i@cantor.mx>
2012-10-08 16:20:04 -0700

    added scrollto directive

--- pnltracker-expjs/private/app/js/directives.js
+++ pnltracker-expjs/private/app/js/directives.js
@@ -6,9 +6,32 @@
 var pnlModule = angular.module('pnlApp.directives', []);
 
 pnlModule.directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) { elm.text(version); };
-  }]);
+  return function(scope, elm, attrs) { elm.text(version); };
+}]);
 pnlModule.directive('appUpload', function () {
-    return {  templateUrl: 'partials/upload.html' };
-  });
+  return {  templateUrl: 'partials/upload.html' };
+});
 
+
+
+pnlModule.directive('scrollTo', function(){
+    return {
+      restrict: 'A',
+      replace: false, 
+      transclude: false, 
+      // The linking function will add behavior to the template
+      link: function(scope, element, attrs) {
+        // Clicking on title should open/close the zippy
+        element.bind('click', fireScroll);
+        if (!attrs.scrollTo) {
+          console.log('missing scrollto');  // _DEBUG
+        }
+        // Toggle the closed/opened state
+        function fireScroll() {
+          debugger;
+          var c = $(attrs.scrollTo).position();
+          if (c) {window.scrollTo(c.left, c.top);}
+        }
+      }
+    }
+  });
