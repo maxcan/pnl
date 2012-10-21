6a9ccae8be92847bf6d490edfa5f9541d47bae20
Max Cantor (MBPr) <i@cantor.mx>
2012-10-20 21:38:23 -0700

    dropt tradedtlctrl

--- pnltracker-expjs/private/app/js/app.js
+++ pnltracker-expjs/private/app/js/app.js
@@ -3,9 +3,9 @@
 angular.module('pnlApp', ['pnlApp.filters', 'pnlApp.services', 'pnlApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', { templateUrl: 'partials/pnlapp.html' });
-    $routeProvider.when('/trades/:tradeId'
-                       , {templateUrl: 'partials/tradeDtl.html'
-                         , controller: TradeDtlCtrl});
+    // $routeProvider.when('/trades/:tradeId'
+    //                    , {templateUrl: 'partials/tradeDtl.html'
+    //                      , controller: TradeDtlCtrl});
     $routeProvider.when('/admin'
                        , {templateUrl: 'partials/admin.html'
                          , controller: AdminCtrl});
